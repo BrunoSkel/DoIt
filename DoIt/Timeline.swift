08b5288cf6629ceb60ee0a6358a6248923ddc609
DaniloMarshall <danilo.marshall@gmail.com>
2015-06-10 10:05:44 -0300

    Initial project sketch
    Initial project sketch

---
+++ DoIt/Timeline.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Danilo S Marshall on 6/10/15.
+//  Copyright (c) 2015 CoffeeTime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DoIt/TimelinePoint.swift
@@ -0,0 +1,51 @@
+//
+//  TimelinePoint.swift
+//  DoIt
+//
+//  Created by Danilo S Marshall on 6/10/15.
+//  Copyright (c) 2015 CoffeeTime. All rights reserved.
+//
+
+//import Foundation
+import UIKit
+
+enum PointState {
+    case Locked
+    case Unfinished
+    case Finished
+}
+
+@IBDesignable class TimelinePoint : UIButton {
+    
+    private let unfineshedChallengeImg = UIImage(named:"TimelineBtn_empty")
+    private let finishedChallengeImg = UIImage(named:"TimelineBtn_filled")
+    private let lockedChallengeImg = UIImage(named:"TimelineBtn_inactive")
+    
+    @IBInspectable var currentState : PointState = PointState.Locked {
+        didSet {
+            updateLayerProperties()
+        }
+    }
+    
+    override func layoutSubviews()
+    {
+        super.layoutSubviews()
+        updateLayerProperties()
+    }
+    
+    private func updateLayerProperties () {
+        
+    switch currentState {
+    case PointState.Locked:
+        setImage(self.lockedChallengeImg, forState: .Normal)
+    case PointState.Unfinished:
+        setImage(self.unfineshedChallengeImg, forState: .Normal)
+    case PointState.Finished:
+        setImage(self.finishedChallengeImg, forState: .Normal)
+    default:
+        setImage(self.lockedChallengeImg, forState: .Normal)
+    }
+        
+    }
+    
+}
