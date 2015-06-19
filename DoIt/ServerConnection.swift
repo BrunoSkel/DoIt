8c18b903858932131713f7829656c9864a4c5b74
DaniloMarshall <danilo.marshall@gmail.com>
2015-06-19 16:06:42 -0300

    Start to fix crash when has no connection
    Start to fix crash when has no connection

--- DoIt/ServerConnection.swift
+++ DoIt/ServerConnection.swift
@@ -78,32 +78,39 @@ class ServerConnection{
             utcDateFormatter.locale = NSLocale(localeIdentifier: "en_US")
             utcDateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
             
-            //Get Date 0 in GMT
-            let serverStringDate0 = days[0]
-            let date0 = utcDateFormatter.dateFromString(serverStringDate0)
+            // Check if had no connection
+            if days.count == 0 {
+                completionHandler(-1,NSDate())
+            }
+            else {
+                //Get Date 0 in GMT
+                let serverStringDate0 = days[0]
+                let date0 = utcDateFormatter.dateFromString(serverStringDate0)
+                
+                //Get Current Date in GMT
+                let serverStringCurrentDate = days[1]
+                let currentDate = utcDateFormatter.dateFromString(serverStringCurrentDate)
+                
+                //Calculates Current Day number
+                var secondsBetween : NSTimeInterval = currentDate!.timeIntervalSinceDate(date0!)
+                var currentDayNumber = Int(secondsBetween / 86400);
+                
+                let localDateFormatter = NSDateFormatter()
+                localDateFormatter.dateStyle = NSDateFormatterStyle.LongStyle
+                localDateFormatter.timeStyle = NSDateFormatterStyle.LongStyle
+                let localDateString = localDateFormatter.stringFromDate(currentDate!)
+                
+                // TEST for future date
+                /*
+                let calendar : NSCalendar = NSCalendar.currentCalendar()
+                let futureDate = calendar.dateByAddingUnit(.CalendarUnitDay, value: 2, toDate: currentDate!, options: nil)
+                let futureDay = currentDayNumber + 2
+                completionHandler(futureDay,futureDate!)
+                */
+                
+                completionHandler(currentDayNumber,currentDate!)
+            }
             
-            //Get Current Date in GMT
-            let serverStringCurrentDate = days[1]
-            let currentDate = utcDateFormatter.dateFromString(serverStringCurrentDate)
-            
-            //Calculates Current Day number
-            var secondsBetween : NSTimeInterval = currentDate!.timeIntervalSinceDate(date0!)
-            var currentDayNumber = Int(secondsBetween / 86400);
-            
-            let localDateFormatter = NSDateFormatter()
-            localDateFormatter.dateStyle = NSDateFormatterStyle.LongStyle
-            localDateFormatter.timeStyle = NSDateFormatterStyle.LongStyle
-            let localDateString = localDateFormatter.stringFromDate(currentDate!)
-        
-            // TEST for future date
-            /*
-            let calendar : NSCalendar = NSCalendar.currentCalendar()
-            let futureDate = calendar.dateByAddingUnit(.CalendarUnitDay, value: 2, toDate: currentDate!, options: nil)
-            let futureDay = currentDayNumber + 2
-            completionHandler(futureDay,futureDate!)
-            */
-            
-            completionHandler(currentDayNumber,currentDate!)
         })
     }
     
@@ -117,11 +124,16 @@ class ServerConnection{
         let queue:NSOperationQueue = NSOperationQueue()
         
         let urlData: Void = NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler:{ (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-            var results : NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
-            if ((error) != nil){
-                println(error)
-            }else{
-                completionHandler(results as String)
+            if(data == nil) {
+                completionHandler("")
+            }
+            else {
+                var results : NSString = NSString(data: data, encoding: NSUTF8StringEncoding)!
+                if ((error) != nil){
+                    println(error)
+                }else{
+                    completionHandler(results as String)
+                }
             }
         })
     }
